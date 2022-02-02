6aa4754cbbbed58aff762ec24c2c94eb381d66e7
Mub <84807110+Mub1532@users.noreply.github.com>
2022-02-02 13:54:53 +0000

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className="fixed h-full w-full">
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
