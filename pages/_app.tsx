b0ad134fa63fc320985a5af38b4a5cadf4b0f2c5
Mub <84807110+Mub1532@users.noreply.github.com>
2022-02-04 20:41:03 +0000

    Updated meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,21 +9,22 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         {/*<!-- Primary Meta Tags -->*/}
         <title>Frontend Development || Mub1532</title>
-        <meta name="title" content="Frontend Development || Mub1532" />
-        <meta name="description" content="Hello, welcome to my site, here you will find all sorts of things such as My Projects, services and much more!" />
+        <meta name="title" content="Google Clone || Projects @ mubkhan.dev" />
+        <meta name="description" content="A Google clone, made by Mub" />
+        <meta name="theme-color" content="#3e8fe6" />
 
         {/*<!-- Open Graph / Facebook -->*/}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.mubkhan.dev/" />
+        <meta property="og:url" content="https://google.mubkhan.dev" />
         <meta property="og:title" content="Frontend Development || Mub1532" />
-        <meta property="og:description" content="Hello, welcome to my site, here you will find all sorts of things such as My Projects, services and much more!" />
+        <meta property="og:description" content="A Google clone, made by Mub" />
         <meta property="og:image" content="https://cdn.discordapp.com/avatars/518720512344457216/cabfd7d8b8af9cb5da61124bac1309da.png" />
 
         {/*<!-- Twitter -->*/}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.mubkhan.dev/" />
+        <meta property="twitter:url" content="https://google.mubkhan.dev" />
         <meta property="twitter:title" content="Frontend Development || Mub1532" />
-        <meta property="twitter:description" content="Hello, welcome to my site, here you will find all sorts of things such as My Projects, services and much more!" />
+        <meta property="twitter:description" content="A Google clone, made by Mub" />
         <meta property="twitter:image" content="https://cdn.discordapp.com/avatars/518720512344457216/cabfd7d8b8af9cb5da61124bac1309da.png" />
 
       </Head>
