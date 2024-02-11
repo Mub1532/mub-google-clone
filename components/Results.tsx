d9578870a114603f1341a6e36592ef23bab24fd7
Mub <84807110+Mub1532@users.noreply.github.com>
2024-02-11 17:52:52 +0000

    fianl commit before archive

--- components/Results.tsx
+++ components/Results.tsx
@@ -3,7 +3,7 @@ import Pages from "./Pages";
 
 function Results({ results }: any) {
     return <div className="flex flex-col text-xs md:text-sm lg:text-base dark:text-zinc-400 mx-auto p-2 px-4 md:pl-40">
-        <div className="mb-4 text-zinc-500">Around {results.searchInformation?.formattedTotalResults} results. Took {results.searchInformation.formattedSearchTime} seconds Note: Results come from Googles API, I not responsible for the results</div>
+        <div className="mb-4 text-zinc-500">Around {results.searchInformation?.formattedTotalResults} results. Took {results.searchInformation.formattedSearchTime} seconds Note: Results come from Googles official API</div>
         {results.items?.map((e: { link: string | undefined; formattedUrl: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; title: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; snippet: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }, i: Key | null | undefined) => {
             return (
                 <div key={i} className="max-w-xl mb-4">
